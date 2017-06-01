ea54671bbf8c6f0b9fb1c11a9657f83c57e842a2
Duc Nghiem Xuan <git@ducnx.com>
2017-06-01 18:50:05 +0900

    Hello Gjallarhorn

---
+++ WpfFs/App.fs
@@ -0,0 +1,12 @@
+﻿open System
+open FsXaml
+open Gjallarhorn.Wpf
+open WpfFs
+
+type App = XAML<"App.xaml">
+type MainWin = XAML<"MainWindow.xaml">
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv = 
+    Framework.runApplication App MainWin Program.application
